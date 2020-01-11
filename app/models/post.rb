48014ef6eb7846fdd8b2a7e17efada808f9af5fe
cmcfadzean <court@kajabi.com>
2020-01-11 10:23:52 -0800

    adding content type

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
   belongs_to :user
   acts_as_votable
   has_attached_file :photo, styles: { lg: "800x800>", md: "500x500>", sm: "300x300>", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
-  validates :photo, :attachment_content_type => { :content_type => ['image/png', 'image/jpg']}
+  validates :photo, :attachment_content_type => { :content_type => ['image/png', 'image/jpg', 'image/jpeg' ]}
 end
